2ca3daab00f97f5fffbeef513ae62a4578ed3492
autonomuscoder <mahiabirgit@tutanota.com>
2024-03-28 19:12:26 +0600

    added bumblebee for i3

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-nitrogen --random ~/walls/ --set-zoom-fill &
-picom --config ~/.config/picom/picom.conf --experimental-backends &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+# nitrogen --random ~/walls/ --set-zoom-fill &
+nitrogen --restore &
+picom --config ~/.config/picom/picom.conf &
 numlockx on &
 dunst &
 nm-applet &
 blueman-applet &
 volumeicon &
 xfce4-power-manager &
+autotiling &
 xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
