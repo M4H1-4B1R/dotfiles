4d1d6f8baaa455c65dffb34c67caf6669c57be5f
autonomuscoder <mahiabirgit@tutanota.com>
2024-04-18 19:46:33 +0600

    modified for arch

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -3,7 +3,7 @@
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 #/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 nitrogen --restore &
-picom &
+picom --config ~/.config/picom/picom.conf &
 cbatticon &
 dunst &
 xclip &
@@ -12,3 +12,4 @@ nm-applet &
 numlockx on &
 xfce4-power-manager &
 sxhkd -c ~/.config/bspwm/sxhkdrc &
+xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
