c11f1d39fc4c441dd9834cad293dca24c6de08f0
autonomuscoder <mahiabirgit@tutanota.com>
2024-04-29 20:51:58 +0600

    some config change for debian

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 ~/.config/bspwm/launch.sh
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-#/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 nitrogen --restore &
 picom --config ~/.config/picom/picom.conf &
 cbatticon &
@@ -10,6 +10,6 @@ xclip &
 volumeicon &
 nm-applet &
 numlockx on &
-xfce4-power-manager &
+#xfce4-power-manager &
 sxhkd -c ~/.config/bspwm/sxhkdrc &
 xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
