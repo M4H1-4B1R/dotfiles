90f7a8d98037c5041c722ab02a6f5ca0dd43c297
autonomuscoder <mahiabirgit@tutanota.com>
2024-05-21 22:49:32 +0600

    restucturing the repo

--- dwm-flexipatch/patch/alttab.h
+++
@@ -1,5 +0,0 @@
-#include <time.h>
-
-static void drawalttab(int nwins, int first, Monitor *m);
-static void alttabstart(const Arg *arg);
-static void alttabend();
--- dwm-flexipatch/patch/focusfollowmouse.h
+++
@@ -1,1 +0,0 @@
-static Client *getpointerclient(void);
