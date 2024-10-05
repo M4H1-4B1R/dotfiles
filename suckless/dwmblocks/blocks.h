8ba405e14fa5a2c19d1435de967d25aebfc6d14f
M4H1-4B1R <mahiabirgit@tutanota.com>
2024-10-05 18:34:57 +0600

    added colors using status2d patch in dwmblocks

--- suckless/dwmblocks/blocks.h
+++ suckless/dwmblocks/blocks.h
@@ -2,15 +2,15 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-	{" ", "~/.local/scripts/kernel",					360,		2},
+	{"^c#f5c2e7^ ", "~/.local/scripts/kernel",					360,		2},
 
-	{"", "~/.local/scripts/cpu",					10,		18},
+	{"^c#cba6f7^ ", "~/.local/scripts/cpu",					10,		18},
 
-	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	10,		0},
+	{"^c#f38ba8^ ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	10,		0},
 
 	{"", "~/.local/scripts/battery",					5,		3},
 
-	{"󱑆 ", "date '+%I:%M%p'",					5,		0},
+	{"^c#fab387^󱑆 ", "date '+%I:%M%p'",					5,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
