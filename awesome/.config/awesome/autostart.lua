40882c642994c252384b9a4c4e07b8e5d744c320
M4H1-4B1R <mahiabirgit@tutanota.com>
2024-10-27 19:33:41 +0600

    added more autostart in awesome

--- awesome/.config/awesome/autostart.lua
+++ awesome/.config/awesome/autostart.lua
@@ -6,11 +6,9 @@ local awful = require("awful")
 --autostart
 awful.spawn.with_shell("picom")
 awful.spawn.with_shell("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
-awful.spawn.with_shell("feh --bg-fill --randomize /home/abir/walls/")
+awful.spawn.with_shell("feh --bg-fill /home/abir/walls/yellow-car.jpg")
 awful.spawn.with_shell("cbatticon")
+awful.spawn.with_shell("nm-applet")
 awful.spawn.with_shell("dunst")
 awful.spawn.with_shell("xclip")
-
-awful.spawn.with_shell(
-	"[ -z '$DBUS_SESSION_BUS_ADDRESS' ] && eval $(/usr/bin/dbus-launch --exit-with-session --sh-syntax) dbus-update-activation-environment --verbose --all"
-)
+awful.spawn.with_shell("xfce4-power-manager")
--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -559,6 +559,7 @@ awful.rules.rules = {
 				"Wpa_gui",
 				"veromix",
 				"xtightvncviewer",
+				"pavucontrol",
 			},
 
 			-- Note that the name property shown in xprop might be set slightly after creation of the client
