45eff895034df86546c4492062b43f572cd572a7
M4H1-4B1R <mahiabirgit@tutanota.com>
2024-10-29 15:57:18 +0600

    added volumeicon and blueman to autostart

--- awesome/.config/awesome/autostart.lua
+++ awesome/.config/awesome/autostart.lua
@@ -9,6 +9,8 @@ awful.spawn.with_shell("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-
 awful.spawn.with_shell("feh --bg-fill /home/abir/walls/yellow-car.jpg")
 awful.spawn.with_shell("cbatticon")
 awful.spawn.with_shell("nm-applet")
+awful.spawn.with_shell("volumeicon")
+awful.spawn.with_shell("blueman-applet")
 awful.spawn.with_shell("dunst")
 awful.spawn.with_shell("xclip")
 awful.spawn.with_shell("xfce4-power-manager")
