293e71fa37887526e414df1a8b664e8d7cbc7d09
M4H1-4B1R <mahiabirgit@tutanota.com>
2024-10-30 19:04:50 +0600

    autostart polkit and powermanager in bspwm

--- bspwm/.config/bspwm/autostart.sh
+++ bspwm/.config/bspwm/autostart.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
 ~/.config/bspwm/launch.sh
-/usr/libexec/polkit-gnome-authentication-agent-1 &
-feh --bg-fill --randomize ~/walls &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+feh --bg-fill ~/walls/pink-waves.jpg &
 picom --config ~/.config/picom/picom.conf &
 dunst &
 xclip &
 volumeicon &
 nm-applet &
 blueman-applet &
-#xfce4-power-manager &
+xfce4-power-manager &
 sxhkd -c ~/.config/bspwm/sxhkdrc &
-brightnessctl set 40%
