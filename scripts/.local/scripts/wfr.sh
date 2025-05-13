7598e6be328429d2b4814a47fa7bff2acf293cbf
M4H1-4B1R <mahiabirgit@tutanota.com>
2025-05-13 20:06:40 +0600

    script for screenrecording in hyprland (wf-recorder)

---
+++ scripts/.local/scripts/wfr.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+pgrep -x "wf-recorder" && pkill -INT -x wf-recorder && notify-send -h string:wf-recorder:record -t 1000 "Finished Recording" && exit 0
+
+notify-send -h string:wf-recorder:record -t 1000 "Recording in:" "<span color='#90a4f4' font='26px'><i><b>3</b></i></span>"
+
+sleep 1
+
+notify-send -h string:wf-recorder:record -t 1000 "Recording in:" "<span color='#90a4f4' font='26px'><i><b>2</b></i></span>"
+
+sleep 1
+
+notify-send -h string:wf-recorder:record -t 950 "Recording in:" "<span color='#90a4f4' font='26px'><i><b>1</b></i></span>"
+
+sleep 1
+
+dateTime=$(date +%m-%d-%Y-%H:%M:%S)
+wf-recorder --bframes max_b_frames -f $HOME/Videos/$dateTime.mp4
