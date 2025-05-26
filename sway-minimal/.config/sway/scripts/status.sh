77e6e947df4967293924c2de509ebb06f69809fb
M4H1-4B1R <mahiabirgit@tutanota.com>
2025-05-26 17:11:11 +0600

    own minimal sway config

---
+++ sway-minimal/.config/sway/scripts/status.sh
@@ -0,0 +1,20 @@
+# The Sway configuration file in ~/.config/sway/config calls this script.
+# You should see changes to the status bar after saving this script.
+# If not, do "killall swaybar" and $mod+Shift+c to reload the configuration.
+
+# Produces "21 days", for example
+uptime_formatted=$(uptime | cut -d ',' -f1 | cut -d ' ' -f4,5)
+
+# The abbreviated weekday (e.g., "Sat"), followed by the ISO-formatted date
+# like 2018-10-06 and the time (e.g., 14:01)
+date_formatted=$(date "+%a %F %H:%M")
+
+# Get the Linux version but remove the "-1-ARCH" part
+linux_version=$(uname -r | cut -d '-' -f1)
+
+# Returns the battery status: "Full", "Discharging", or "Charging".
+battery_status=$(cat /sys/class/power_supply/BAT0/status)
+
+# Emojis and characters for the status bar
+# 💎 💻 💡 🔌 ⚡ 📁 \|
+echo ↑ $uptime_formatted  $linux_version 󰂂 $battery_status  $date_formatted
